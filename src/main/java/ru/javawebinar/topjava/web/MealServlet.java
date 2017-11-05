491af338e104f922f86b4d7943d3833610f399dc
MiSh <micangel@mail.ru>
2017-11-05 23:48:53 +0700

    My-HW1, make MealWithExceed list from Meal list

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,18 +1,23 @@
 package ru.javawebinar.topjava.web;
 
-import ru.javawebinar.topjava.dao.MealWithExceedListDB;
+import ru.javawebinar.topjava.dao.MealDB;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;;
 
 public class MealServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
 
-        request.setAttribute("meal", MealWithExceedListDB.mealWithExceeds);
+        List<MealWithExceed> mealWithExceeds = MealsUtil.getFilteredWithExceeded(MealDB.meals, LocalTime.MIN, LocalTime.MAX, 2000);
+        request.setAttribute("meal", mealWithExceeds);
         getServletContext().getRequestDispatcher("/meals.jsp").forward(request, resp);
     }
 }
